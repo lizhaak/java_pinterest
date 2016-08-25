c1501efbc3dd3b56a11ec85119aa0ed8f0f52d9d
Liz Haakenson <lhaakens3@gmail.com>
2016-08-24 22:08:54 -0500

    used a function to print out a string

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,14 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello world!");
+		HelloWorld myReview = new HelloWorld();
+		myReview.exampleMethod("print this out using exampleMethod function");
+	}
+	
+	public void exampleMethod(String message) {
+		System.out.println(message);
 	}
 
 }
+
+
