073373bfdd47e77f637789366279f4c69616e4ab
Liz Haakenson <lhaakens3@gmail.com>
2016-08-25 22:08:18 -0500

    created an array of strings and used a for loop, to print them out separately

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,11 +5,24 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		HelloWorld myReview = new HelloWorld();
 		myReview.exampleMethod("print this out using exampleMethod function");
+		String[] interests = new String[5];
+		interests[0] = "Houses";
+		interests[1] = "New Technology";
+		interests[2] = "Baked Goods";
+		interests[3] = "Hockey";
+		interests[4] = "Ireland";
+		
+		for(int i = 0; i < interests.length; i++) {
+			System.out.println(interests[i]);
+		}
 	}
 	
 	public void exampleMethod(String message) {
 		System.out.println(message);
 	}
+	
+	
+	
 
 }
 
