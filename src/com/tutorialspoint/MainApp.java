2af3afbc7a5cde290881121776460ea3abbe54cd
Liz Haakenson <lhaakens3@gmail.com>
2016-10-22 21:52:32 -0500

    for loops and for each loops

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -1,7 +1,11 @@
 package com.tutorialspoint;
 
+import java.lang.reflect.Array;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.HashMap;
 
 public class MainApp {
    public static void main(String[] args) {
@@ -11,5 +15,18 @@ public class MainApp {
       HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
 
       obj.getMessage();
+      
+//      ArrayList<Integer> weeklyTemperatures = new ArrayList<Integer>();
+//      weeklyTemperatures.add(78);
+//      weeklyTemperatures.add(67);
+//      weeklyTemperatures.add(89);
+//      weeklyTemperatures.add(94);
+//      
+//      for (Integer temperature : weeklyTemperatures) {
+//		System.out.println(temperature);
+//      }
+      
+      
    }
+   
 }
